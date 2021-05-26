6e22aae2f83cedced90dd6f05e0ec6a6c0ecf7cc
AnsifDev <84790890+AnsifDev@users.noreply.github.com>
2021-05-26 23:37:21 +0530

    Add files via upload

---
+++ js/sw.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", function (e){
+	e.waitUtil(caches.open("static").then(function (cache){
+		return cache.addAll([
+		"/MainActivity.html",
+		"/css/styles.css",
+		"/js/swLoader.js",
+		"https://ansifdev.github.io/MyWeb/img512.png"]);
+	}));
+});
+
+self.addEventListener("fetch", function (e){
+	e.respondWith(caches.match(e.request).then(function (res){
+		return res || fetch(e.request);
+	}));
+});
---
+++ js/swLoader.js
@@ -0,0 +1,8 @@
+if ("serviceWorker" in navigator){
+	navigator.serviceWorker.register("js/sw.js")
+	.then(function (e){
+		alert("Response Ok");
+	}).catch(function (e){
+		alert("Response failed"+e);
+	});
+}
