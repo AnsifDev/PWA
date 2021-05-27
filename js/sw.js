469ef225f206a33fdb4ebe914a0cf7e07e5ca282
AnsifDev <84790890+AnsifDev@users.noreply.github.com>
2021-05-27 15:26:43 +0530

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,16 +1,24 @@
-const cacheName = 'static';
-const precacheResources = ['/', '/index.html', '/css/styles.css', '/js/swLoader.js', '/MainActivity.html'];
-self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
+self.addEventListener("install", function(e){
+  e.waitUntil(caches.open("static")
+    .then(function(c){
+      c.addAll(['./index.html', './css/styles.css', './js/swLoader.js', './MainActivity.html']);
+      console.log("Error occured"+err);
+    })
+    .catch(function(err){
+      console.log("Error occured"+err);
+  );
 });
 
 self.addEventListener('fetch', (event) => {
+    console.log("Fetch request received");
     event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
       if (cachedResponse) {
+        console.log("Returned cache data");
         return cachedResponse;
       }
+      console.log("loading from server");
       return fetch(event.request);
-    }),
+    })
   );
 });
