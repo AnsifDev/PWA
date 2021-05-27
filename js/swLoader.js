204a73c01041fcecb6f3b43ba32d4bf6b479f182
AnsifDev <84790890+AnsifDev@users.noreply.github.com>
2021-05-27 15:44:07 +0530

    Update swLoader.js

--- js/swLoader.js
+++ js/swLoader.js
@@ -1,5 +1,5 @@
 if ("serviceWorker" in navigator){
-	navigator.serviceWorker.register("./js/sw.js")
+	navigator.serviceWorker.register("js/sw.js")
 	.then(function (e){
 		alert("Response Ok");
 	}).catch(function (e){
