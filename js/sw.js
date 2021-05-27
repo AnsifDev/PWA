73691303eb4de98772836fa0cb9dd59bffeb7225
AnsifDev <84790890+AnsifDev@users.noreply.github.com>
2021-05-27 08:43:30 +0530

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,12 +1,16 @@
-self.addEventListener("install", e=>{
-    alert("ServiceWorker first load suceeded");
-    e.waitUntil(caches.open("static").then(cache=>{
-        return cache.addAll(["/MainActivity.html", "/index.html"]);
-    }))
-})
-self.addEventListener("fetch", e=>{
-    e.respondWith(caches.match(e.request).then(req=>{
-        if (req) { return req; }
-        else { return fetch(e.request); }
-    }))
-})
+const cacheName = 'static';
+const precacheResources = ['/', '/index.html', '/css/styles.css', '/js/swLoader.js', '/MainActivity.html'];
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    }),
+  );
+});
