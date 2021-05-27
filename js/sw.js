3bbb700f171284a2f0bad487cd81217d073eb7d6
AnsifDev <84790890+AnsifDev@users.noreply.github.com>
2021-05-27 07:27:59 +0530

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,12 +1,12 @@
 self.addEventListener("install", e=>{
 	e.waitUtil(caches.open("static").then(catch=>{
 		return cache.addAll(["./index.html", "./MainActivity.html", "./css/styles.css", "./js/swLoader.js", "https://ansifdev.github.io/MyWeb/img512.png"]);
-	}));
-});
+	}))
+})
 
 self.addEventListener("fetch", e=>{
 	e.respondWith(caches.match(e.request).then(req=>{
              if (req) { return req; }
              else { return fetch(e.request); }
-	}));
-});
+	}))
+})
