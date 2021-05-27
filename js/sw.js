c339b55d3579c1d536700e8a9959bce1f639d5b2
AnsifDev <84790890+AnsifDev@users.noreply.github.com>
2021-05-27 07:39:50 +0530

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", e=>{
     alert("ServiceWorker first load suceeded");
     e.waitUntil(caches.open("static").then(cache=>{
-        return cache.addAll(["./MainActivity.html", "./js/swLoader.js", "./css/styles.css"./index.html"]);
+        return cache.addAll(["./MainActivity.html", "./js/swLoader.js", "./css/styles.css", "./index.html"]);
     }))
 })
 self.addEventListener("fetch", e=>{
