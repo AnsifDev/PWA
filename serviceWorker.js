1b18669578f7170faa464382e1bd0fc5832e62b3
AnsifDev <84790890+AnsifDev@users.noreply.github.com>
2021-05-28 21:37:19 +0530

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,24 +1,24 @@
-self.addEventListener("install", e=>{
+self.addEventListener("install", function(e){
   e.waitUntil(caches.open("static")
     .then(c=>{
       console.log("Everything perfect");
       return c.addAll(["./", "index.html", "js/index.js", "html/MainActivity.html", "https://ansifdev.github.io/MyWeb/img512.png", "https://ansifdev.github.io/MyWeb/img64.png"]);
     })
-    .catch(err=>{
+    .catch(function(err){
       console.log("Error occured"+err);
     })
   )
 })
 
-self.addEventListener("fetch", e=> {
+self.addEventListener("fetch", function(e) {
     console.log("Fetch request received: "+e.request.url);
     e.respondWith(
     caches.match(e.request).then(res=>{
-      if (res) {
-        console.log("Returned cache data");
-        return res;
-      }
-      console.log("loading from server");
-      return fetch(e.request);
+	if (res) {
+	     console.log("Returned cache data");
+	     return res;
+        }
+	console.log("loading from server");
+	return fetch(e.request);
     }));
 })
