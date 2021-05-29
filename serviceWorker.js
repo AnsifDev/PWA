f383fa881d669c0fc1acf0a00605768465ea4619
AnsifDev <84790890+AnsifDev@users.noreply.github.com>
2021-05-29 13:35:57 +0530

    update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -10,7 +10,7 @@ self.addEventListener("install", function(e){
   )
 })
 
-self.addEventListener("fetgch", function(e) {
+self.addEventListener("fetching", function(e) {
     console.log("Fetch request received: "+e.request.url);
     e.respondWith(
     caches.match(e.request).then(function(res){
